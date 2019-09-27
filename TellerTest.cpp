10cdcd89b6709e268ad08eef9bb3c6000ea04ef5
Nerd-1 <ooyendyk@tuta.io>
2019-09-27 09:37:04 +0930

    Improved UML. Added User, Client and Teller tests

---
+++ TellerTest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <assert.h>
+#include "User.h"
+#include "Teller.h"
+#include "Client.h"
+
+int main() {
+	try {
+		teller0 Teller("Chad Channing");
+	} catch {
+		std::cout << "Error: Looks like theres a problem with the initializer" << std::endl;
+		teller0 Teller("Chad Channing");
+	}
+
+	try {
+		client0 Client("Kirst Novoselic");
+	} catch {
+		std::cout << "Error: There was a problem initializing Client. You'll need to fix this for clientSummary() to work" << std::endl;
+		client0 Client("Kirst Novoselic");
+	}
+
+	//clientSummary() requires manual testing.
+}
